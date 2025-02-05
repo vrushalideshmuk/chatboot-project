1c80a7dfdb8780c9a3dacaf4cfd2d82e62832e3b
Vrushali Deshmukh <84955649+vrushalideshmuk@users.noreply.github.com>
2025-02-05 20:17:47 +0530

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ChatComponent from './components/ChatComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Chatbot for Supplier & Product Information</h1>
+      <ChatComponent />
+    </div>
+  );
+}
+
+export default App;
